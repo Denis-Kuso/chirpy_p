feb0306dbb7b1916386f51045d9f6a2cb3d22393
Denis Kuso <sourcework.online@keemail.me>
2023-10-14 11:28:06 +0100

    make plain vanila server

--- main.go
+++ main.go
@@ -1,9 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+    "log"
+)
 
-import "fmt"
+//build custom handler
+/*
+Your handler should do the following:
+
+    Write the Content-Type header
+    Write the status code using w.WriteHeader
+    Write the body text using w.Write
+*/
 
 func main() {
-    fmt.println("Chirp chirp...")
+    const portNum = "8080"
+    fmt.Println("Spinning up")
+
+    rootPath := "." // home for now
+    readinessEndpoint := "/healthz"
+
+    mux := http.NewServeMux()
+    corsMux := middlewareCors(mux)
+
+    mux.Handle("/",http.FileServer(http.Dir(rootPath)))
+    server := &http.Server{
+        Addr:   ":" + portNum,
+        Handler: corsMux,
+        MaxHeaderBytes: 1 << 20,
+        }
+    log.Printf("Serving on port: %s\n",portNum)
+    server.ListenAndServe()
 }
 
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
