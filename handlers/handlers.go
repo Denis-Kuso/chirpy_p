aa56b56533c8b9b0f806ff33c133eb998b087907
Denis Kuso <sourcework.online@keemail.me>
2023-10-26 14:23:30 +0100

    add support for JSON io at chirp length validation

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,35 +5,40 @@ import (
     "log"
     "encoding/json"
     "fmt"
-    //"io"
+    "io"
 )
 
 func ValidateChirp(w http.ResponseWriter, r *http.Request) {
 
-/*Add a new endpoint to the Chirpy API that accepts a POST request at /api/validate_chirp. It should expect a JSON body of this shape:
-{
-    "body": "This is an opinion I need to share with the world"
-}*/
-// example
-//func handler(w http.ResponseWriter, r *http.Request){
-    // ...
-    /* decode incoming body data into a struct containing only the text
-    I ll ignore other data for now
-    give feedback irespective of status code in form of json*/
-    const charLimit = 14
+    const charLimit = 140
     var statusCode int
-    resp := make(map[string]string)
-    req := make(map[string]string)
-    req["message"] = "HHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendello My friend"
-    var feedback string
-    if len(req["message"]) > charLimit {
+    type requestBody struct {
+        Message string `json:"body"`
+    }
+    type responseBody struct {
+        Valid bool `json:"valid"`
+    }
+
+    data, err := io.ReadAll(r.Body)
+    if err != nil {
+        fmt.Println("Oh well, mistake reading response")
+        return 
+    }
+    params := requestBody{}
+    err = json.Unmarshal(data, &params)
+    if err != nil {
+        fmt.Println("Oh well, mistake unmarshaling")
+        return 
+    }
+
+    resp := responseBody{}
+    if len(params.Message) > charLimit {
         statusCode = 400
-        feedback = "Too long my friend"
+        resp.Valid = false
     }else {
         statusCode = 200
-        feedback = "all good"
+        resp.Valid = true 
     }
-    resp["message"] = feedback
     jsonResp, err := json.Marshal(resp)
 	if err != nil {
 		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
