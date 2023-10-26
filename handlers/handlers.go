666a8b37e97abdc11a9618be960d0ca9aee7c719
Denis Kuso <sourcework.online@keemail.me>
2023-10-26 13:51:14 +0100

    add first draft of validating chirp length

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,11 +4,11 @@ import (
     "net/http"
     "log"
     "encoding/json"
+    "fmt"
+    //"io"
 )
 
 func ValidateChirp(w http.ResponseWriter, r *http.Request) {
-
-//At Chirpy, we have a silly rule that says all Chirps must be 140 characters long or less.
 
 /*Add a new endpoint to the Chirpy API that accepts a POST request at /api/validate_chirp. It should expect a JSON body of this shape:
 {
@@ -20,66 +20,25 @@ func ValidateChirp(w http.ResponseWriter, r *http.Request) {
     /* decode incoming body data into a struct containing only the text
     I ll ignore other data for now
     give feedback irespective of status code in form of json*/
-    const charLimit = 140
+    const charLimit = 14
     var statusCode int
-    type clientData struct {
-        text string
-    }
-    type userResponse struct {
-        status bool `json:"valid"`
-    }
-    dec := json.NewDecoder(r.Body)
-    cData := clientData{}
-    err := dec.Decode(&cData)
-    if err != nil {
-        errMessage := "err: something went wrong"
-        log.Printf("Error decoding parameters: %s", err)
-        w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(500)
-        w.Write([]byte(string(errMessage)))
-		return
-    }
-    resp := userResponse{}
-    err = json.NewEncoder(w).Encode(resp)
-    if err != nil {
-                http.Error(w, err.Error(), 500)
-                return
-    }
-    // exceed limit
-    if len(cData.text) > charLimit {
-        resp.status = false
+    resp := make(map[string]string)
+    req := make(map[string]string)
+    req["message"] = "HHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendHello My friendello My friend"
+    var feedback string
+    if len(req["message"]) > charLimit {
         statusCode = 400
-    } else {
+        feedback = "Too long my friend"
+    }else {
         statusCode = 200
-        resp.status = true
+        feedback = "all good"
     }
-    data, _ := json.Marshal(resp)
-    w.Header().Set("Content-Type", "application/json")
+    resp["message"] = feedback
+    jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
     w.WriteHeader(statusCode)
-    w.Write(data)
+    w.Header().Set("Content-type", "application/json")
+    w.Write(jsonResp)
 }
-    
-
-
-//func handler(w http.ResponseWriter, r *http.Request){
-//    // ...
-//
-//    type returnVals struct {
-//        // the key will be the name of struct field unless you give it an explicit JSON tag
-//        CreatedAt time.Time `json:"created_at"`
-//        ID int `json:"id"`
-//    }
-//    respBody := returnVals{
-//        CreatedAt: time.Now(),
-//        ID: 123,
-//    }
-//    dat, err := json.Marshal(respBody)
-//	if err != nil {
-//		log.Printf("Error marshalling JSON: %s", err)
-//		w.WriteHeader(500)
-//		return
-//	}
-//    w.Header().Set("Content-Type", "application/json")
-//    w.WriteHeader(200)
-//	w.Write(dat)
-//}
---
+++ handlers/handlers_test.go
@@ -0,0 +1,132 @@
+package handlers
+
+import (
+    "testing"
+    "net/http"
+    "net/http/httptest"
+    "io/ioutil"
+    "fmt"
+    "strings"
+)
+
+type UserFeedback struct {
+    valid bool `json:"valid"`
+}
+type Test struct {
+    name string
+    server *httptest.Server
+    isValid bool
+    expStatusCode int
+    expBody string
+}
+
+func TestValidateChirp(t *testing.T) {
+    tests := []Test{
+        {"basic-request",
+        httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter,
+        r *http.Request) {
+            w.WriteHeader(http.StatusOK)
+            w.Write([]byte(`{ "body": "This is an opinion I need to share with the world"}`)) 
+        })),
+        true,
+        200,
+        `"valid": true`,
+    },
+    {"invalid body length",
+    httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter,
+        r *http.Request) {
+            w.WriteHeader(http.StatusOK)
+            w.Write([]byte(`{ "body": "This is an opinion I need to share with the world, so badly that I am willing to write soooooo man chaaarsss to share it with everyone, like yeah, so that an actual error will pop out."}`))
+        })),
+    false,
+    400,
+    `"valid":false`,
+    }}
+    for _, test := range tests {
+		t.Run(test.name, func(t *testing.T){
+			defer test.server.Close()
+            //implement
+            req, _ := http.Get(test.server.URL)
+            body, _ := ioutil.ReadAll(req.Body)
+            if test.expStatusCode != req.StatusCode {
+                t.Errorf("FAILED with %s. Wrong status code! Expected: %d, got: %d",test.name, test.expStatusCode, req.StatusCode)
+            }
+            fmt.Println(body)
+        })
+    }
+// test when request data is ok
+// test when request data has text of invalid text length
+// test error on our side
+}
+
+func TestHandlerCheckStatus(t *testing.T) {
+    server := httptest.NewServer(http.HandlerFunc(CheckStatus))
+    resp, err := http.Get(server.URL)
+    if err != nil {
+        t.Fatalf("err: %v",err)
+    }
+    // status
+    if resp.StatusCode != http.StatusOK {
+        t.Errorf("Err: expected %d, recevied: %d", resp.StatusCode, http.StatusOK)
+    }
+    defer resp.Body.Close()
+
+    // body
+    expectedText := "OK"
+    b, err := ioutil.ReadAll(resp.Body)
+    if expectedText != string(b) {
+        t.Fatalf("err: expected: %s, got: %s", expectedText, string(b))
+    }
+}
+
+func TestResetViews(t *testing.T) {
+    s := apiState {
+        ViewCount:10, // to test reseting
+    }
+    server := httptest.NewServer(http.HandlerFunc(s.resetViews))
+    resp, err := http.Get(server.URL)
+    if err != nil {
+        t.Fatalf("err: %v",err)
+    }
+    // status
+    if resp.StatusCode != http.StatusOK {
+        t.Errorf("Err: expected %d, recevied: %d", resp.StatusCode, http.StatusOK)
+    }
+    defer resp.Body.Close()
+
+    expectedCount := 0
+    if s.ViewCount != expectedCount {
+        t.Errorf("Err: expected %d, got: %d", expectedCount, s.ViewCount)
+    }
+
+    expectedReply := "Hits reset to 0"
+    b, err := ioutil.ReadAll(resp.Body)
+    if expectedReply != string(b) {
+        t.Fatalf("err: expected: %s, got: %s", expectedReply, string(b))
+    }
+    // test reseting
+}
+
+func TestShowPageViews(t *testing.T) {
+    s := apiState {
+        ViewCount:0,
+    }
+    server := httptest.NewServer(http.HandlerFunc(s.showPageViews))
+    resp, err := http.Get(server.URL)
+    if err != nil {
+        t.Fatalf("err: %v",err)
+    }
+    // status
+    if resp.StatusCode != http.StatusOK {
+        t.Errorf("Err: expected %d, recevied: %d", resp.StatusCode, http.StatusOK)
+    }
+    defer resp.Body.Close()
+
+    expectedBody := "Welcome, Chirpy Admin"
+
+    b, err := ioutil.ReadAll(resp.Body)
+    if !strings.Contains(string(b), expectedBody) {
+        t.Fatalf("err: expected body to contain: %s, got: %s", expectedBody, string(b))
+    }
+}
+
