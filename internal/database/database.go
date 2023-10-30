d30b02e795b92bfc2c1f9eb8bd2438e0f58322f9
Denis Kuso <sourcework.online@keemail.me>
2023-10-30 11:21:59 +0000

    revise first draft of json database

--- internal/database/database.go
+++ internal/database/database.go
@@ -26,6 +26,7 @@ type DBStructure struct {
 func NewDB(path string) (*DB, error){
     db := DB{
         path: path,
+        mu:   &sync.RWMutex{},
         // initialise with unlocked RWmutex
     }
     return &db, nil
@@ -33,9 +34,26 @@ func NewDB(path string) (*DB, error){
 
 // CreateChirp creates a new chirp and saves it to disk
 func (db *DB) CreateChirp(body string) (Chirp, error){
-    // how to determine id? BY length of DBStructure.Chirps?
-    return Chirp{}, nil
+    dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	id := len(dbStructure.Chirps) + 1
+	chirp := Chirp{
+		ID:   id,
+		Body: body,
+	}
+	dbStructure.Chirps[id] = chirp
+
+	err = db.writeDB(dbStructure)
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	return chirp, nil
 }
+
 
 // GetChirps returns all chirps in the database
 func (db *DB) GetChirps() ([]Chirp, error) {
@@ -55,36 +73,48 @@ func (db *DB) GetChirps() ([]Chirp, error) {
 
 func (db *DB) ensureDB() error {
     // create a file if it does not exist
-    if _, err := os.Stat(db.path); errors.Is(err, os.ErrNotExist) {
+    _, err := os.Stat(db.path)
+    if errors.Is(err, os.ErrNotExist) {
         fmt.Println(err)
         file,_ := os.Create(db.path)
         fmt.Println("created",file)
         return nil
     }
-        //if _, createErr := os.Create(db.path){
-            //return createErr
-        //}
-    return nil
+    return err 
 }
 
 // loadDB reads the database file into memory
 func (db *DB) loadDB() (DBStructure, error) {
-//    err := db.ensureDB()
-//    if err != nil {
-//        fmt.Printf("Err loading database: %v\n",err)
-//        return DBStructure{}, err
-//    }
-//    data, readErr := os.ReadFile(db.path)
-//	if err != nil {
-//		log.Fatal(err)
-//        return DBStructure{}, readErr
-//	}
-//    return DBStructure{string(data)}, nil
-return DBStructure{make(map[int]Chirp)},nil
+    db.mu.RLock()
+	defer db.mu.RUnlock()
+
+	dbStructure := DBStructure{}
+	data, err := os.ReadFile(db.path)
+	if errors.Is(err, os.ErrNotExist) {
+		return dbStructure, err
+	}
+	err = json.Unmarshal(data, &dbStructure)
+	if err != nil {
+		return dbStructure, err
+	}
+
+	return dbStructure, nil
 }
+
 
 // writeDB writes the database file to disk
 func (db *DB) writeDB(dbStructure DBStructure) error {
+    db.mu.Lock()
+    defer db.mu.Unlock()
+    data, err := json.Marshal(dbStructure)
+    if err != nil {
+        return err
+    }
+    err = os.WriteFile(db.path, data, 0600)
+    if err != nil {
+        return err
+    }
+
     return nil
 }
 
